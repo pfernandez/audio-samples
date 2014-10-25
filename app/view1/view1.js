6c56197b53703bb5e946c07ee26e1e5e3aca339c
Paul Fernandez <paul4nandez@gmail.com>
2014-10-24 22:25:55 -0700

    Got data

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -3,50 +3,45 @@
 angular.module('myApp.view1', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
+	$routeProvider.when('/view1', {
+		templateUrl: 'view1/view1.html',
+		controller: 'View1Ctrl'
+	});
 }])
 
 .controller('View1Ctrl', ['$scope', function($scope) {
-  
-  $scope.message1 = 'angular sux';
+	$scope.message1 = 'angular sux';
 }])
 
 .controller('FetchController', ['$scope', '$http', '$templateCache',
-  function($scope, $http, $templateCache) {
-    $scope.method = 'GET';
-    $scope.url = 'http://www.freesound.org/apiv2/search';
-    $scope.queryType = 'text';
-    $scope.query = 'piano';
-    $scope.url = $scope.url + '/' + $scope.queryType + '/?query=' + $scope.query;
-    $http.defaults.headers.common.Authorization = 
-      'Token 9b72591754173d4d8baecbfb4f410c7bad47c138';
-    $scope.data = null;
+function($scope, $http, $templateCache) {
 
-    var fetch = function() {
-      $scope.code = null;
-      $scope.response = null;
-      $http({
-          method: $scope.method, 
-          url: $scope.url,
-          cache: $templateCache
-        }).
-        success(function(data) {
-        console.log(data);
-          return data;
-          //$scope.jsonString = JSON.stringify(data, undefined, 2);
-        }).
-        error(function(data, status) {
-          return "Request failed: " + status;
-      });
-    };
+	$http.defaults.headers.common.Authorization = 
+		'Token 9b72591754173d4d8baecbfb4f410c7bad47c138';
+	$scope.query = 'piano';
+	$scope.data = {};
 
-    $scope.updateModel = function(url) {
-      if(url) $scope.url = url;
-      var data = fetch();
-      
-      $scope.data = data;
-    };
-  }]);
+	$scope.fetch = function(url, updateModel) {
+		$http.get(url, {cache: $templateCache}).success(function(response) {
+		console.log(response);
+			if(updateModel !== false) {
+				$scope.data = _.omit(response, 'results');
+				$scope.data.results = [];
+				_.each(response.results, function(element, index) {
+					$http.get('http://www.freesound.org/apiv2/sounds/' 
+						+ element.id + '/').success(function(result) {
+						$scope.data.results[index] = result;
+					});
+				});
+			}
+		}).error(function(response, status) {
+			console.log(status + " - Request failed: " + response);
+		});
+	};
+
+	$scope.searchText = function() {
+		$scope.fetch('http://www.freesound.org/apiv2/search/text/?query='
+			+ $scope.query);
+	};
+}
+]);
