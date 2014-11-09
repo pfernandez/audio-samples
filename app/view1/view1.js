cc1cff3278b4b6be89597e04bf5fbeade87b657a
Paul Fernandez <paul4nandez@gmail.com>
2014-11-08 20:22:21 -0800

    Basic functionalityin place. Dubbed SOUNDS RIGHT HERE.

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -3,28 +3,28 @@
 angular.module('myApp.view1', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-	$routeProvider.when('/view1', {
+	$routeProvider.when('/', {
 		templateUrl: 'view1/view1.html',
-		controller: 'View1Ctrl'
+		controller: 'FetchSounds'
 	});
 }])
 
-.controller('View1Ctrl', ['$scope', function($scope) {
-	$scope.message1 = 'angular sux';
-}])
-
-.controller('FetchController', ['$scope', '$http', '$templateCache',
+.controller('FetchSounds', ['$scope', '$http', '$templateCache',
 function($scope, $http, $templateCache) {
 
 	$http.defaults.headers.common.Authorization = 
 		'Token 9b72591754173d4d8baecbfb4f410c7bad47c138';
-	$scope.query = 'piano';
+	$scope.query = '';
 	$scope.data = {};
 
 	$scope.fetch = function(url, updateModel) {
 		$http.get(url, {cache: $templateCache}).success(function(response) {
-		console.log(response);
 			if(updateModel !== false) {
+				$scope.data = response;
+				
+				// TODO: to remove name extensions: x.replace(/\.[^/.]+$/, "")
+				// TODO: Add download buttons
+			/*
 				$scope.data = _.omit(response, 'results');
 				$scope.data.results = [];
 				_.each(response.results, function(element, index) {
@@ -33,6 +33,7 @@ function($scope, $http, $templateCache) {
 						$scope.data.results[index] = result;
 					});
 				});
+				*/
 			}
 		}).error(function(response, status) {
 			console.log(status + " - Request failed: " + response);
@@ -41,7 +42,12 @@ function($scope, $http, $templateCache) {
 
 	$scope.searchText = function() {
 		$scope.fetch('http://www.freesound.org/apiv2/search/text/?query='
-			+ $scope.query);
+			+ $scope.query + '&fields=name,url,previews,tags,username');
 	};
 }
-]);
+]).
+filter('trusted', ['$sce', function ($sce) {
+    return function(url) {
+        return $sce.trustAsResourceUrl(url);
+    };
+}]);
