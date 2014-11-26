788b893ce651ab211ed27a61824590414505c2e0
Paul Fernandez <paul4nandez@gmail.com>
2014-11-25 22:54:28 -0800

    Tag links, results numbering, CSS.

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -13,7 +13,7 @@ angular.module('audio-samples.view1', ['ngRoute', 'ngAudio'])
 function($scope, $http, $templateCache, ngAudio) {
 
 	$http.defaults.headers.common.Authorization = 
-		'Token XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+		'Token 9b72591754173d4d8baecbfb4f410c7bad47c138';
 	$scope.query = '';
 	$scope.data = {};
 	
@@ -21,17 +21,24 @@ function($scope, $http, $templateCache, ngAudio) {
 		$http.get(url, {cache: $templateCache}).success(function(response) {
 			if(updateModel !== false) {
 				$scope.data = response;
-				//TODO: remove name extensions: x.replace(/\.[^/.]+$/, "")
-				//TODO: number each result accurately regardless of page
+				
+				// Add a field indicating what sound # the array begins with.
+				var prevUrl = response.previous;
+				$scope.data.start = response.results.length
+					* (prevUrl ? getQueryVar(prevUrl, 'page') : 0);
+					
+				//TODO: link tags
 			}
 		}).error(function(response, status) {
 			console.log(status + " - Request failed: " + response);
 		});
 	};
 
-	$scope.searchText = function() {
+	$scope.searchText = function(text) {
+		text = text || $scope.query;
+		//TODO: multi-word search
 		$scope.fetch('http://www.freesound.org/apiv2/search/text/?query='
-			+ $scope.query + '&fields=name,url,previews,tags,username');
+			+ text + '&fields=name,url,previews,tags,username');
 	};
 	
 	$scope.play = function() {
@@ -43,8 +50,9 @@ function($scope, $http, $templateCache, ngAudio) {
 			sound.audio.paused = true;
 		}
 		var audio = sound.audio;
+		//TODO: Stop playing when search button is clicked
 		audio.paused ? audio.play() : audio.pause();
-	}
+	};
 	
 	$scope.paused = function() {
 		var paused = this.sound.audio.paused;
@@ -53,6 +61,17 @@ function($scope, $http, $templateCache, ngAudio) {
 		} else {
 			return false;
 		}
+	};
+	
+	function getQueryVar(url, variable) {
+		var vars = url.split("&");
+		for (var i=0; i<vars.length; i++) {
+			var pair = vars[i].split("=");
+			if(pair[0] == variable) {
+				return pair[1];
+			}
+		}
+		return false;
 	}
 	
 }])
@@ -63,11 +82,23 @@ function($scope, $http, $templateCache, ngAudio) {
     };
 }])
 
+.filter('numberWithCommas', function () {
+    return function(number) {
+        return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    };
+})
+
 .filter("asMinutes", function () {
     return function(time) {
     	var time = time || 0,
 			minutes = Math.floor(time / 60),  
 			seconds = Math.floor(time - minutes * 60);
         return minutes + ':' + (seconds > 9 ? seconds : '0' + seconds);
+    }
+})
+
+.filter("noExtension", function () {
+    return function(name) {
+        return name.replace(/\.[^/.]+$/, "");
     }
 });
